e53594fcf6b31bf63396d861f468e5d1e2f66333
Rémi Prévost <remi@exomel.com>
2009-08-16 19:32:14 +0000

    fixed #6, hopefully!

--- themes/default/index.tpl.php
+++ themes/default/index.tpl.php
@@ -1,11 +1,9 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 	<head>
-
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title><?=PUBWICH_TITLE?></title>
 		<link rel="stylesheet" media="screen" href="<?=Pubwich::getThemeUrl()?>/style.css" type="text/css">
-		
 	</head>
 	<body>
 		<div id="wrap">

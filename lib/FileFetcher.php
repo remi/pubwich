6cae060ba98b726e4aa0f06b7d70f11a9faadb74
Rémi Prévost <remi@exomel.com>
2010-02-07 09:33:32 -0500

    updated cURL user-agent

--- lib/FileFetcher.php
+++ lib/FileFetcher.php
@@ -47,7 +47,7 @@
 			curl_setopt ($ch, CURLOPT_URL, $url);
 			curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-			curl_setopt ($ch, CURLOPT_USERAGENT, "Pubwich ".PUBWICH_VERSION." - http://www.pubwich.com/");
+			curl_setopt ($ch, CURLOPT_USERAGENT, "Pubwich ".PUBWICH_VERSION." - http://www.pubwich.org/");
 			$file_contents = curl_exec($ch);
 			curl_close($ch);
 			return $file_contents; 

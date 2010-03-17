8f9dec759f60a7fa078cae9cf872bf32f6822cf1
Rémi Prévost <remi@exomel.com>
2010-03-17 12:45:05 -0400

    Split Github service in two methods: GithubRecentActivity and GithubRepositories

--- lib/Services/Github.php
+++ lib/Services/Github.php
@@ -4,14 +4,14 @@
 	/**
 	 * @classname GitHub
 	 * @description Fetch GitHub user public activity feed
-	 * @version 1.0 (20100107)
+	 * @version 1.1 (20100317)
 	 * @author Rémi Prévost (exomel.com)
-	 * @methods None
+	 * @methods GithubRecentActivity GithubRepositories
 	 */
 
 	Pubwich::requireServiceFile( 'Atom' );
-	class Github extends Atom {
-	
+	class GithubRecentActivity extends Atom {
+
 		/**
 		 * @constructor
 		 */
@@ -28,6 +28,60 @@
 		public function populateItemTemplate( &$item ) {
 			return parent::populateItemTemplate( $item ) + array(
 			);
+		}
+
+		/**
+		 * Adds "github" to the box HTML class attribute
+		 * @return array
+		 */
+		public function populateBoxTemplate( $data ) {
+			return parent::populateBoxTemplate( $data ) + array( 'class' => $data['class'].' github');
+		}
+
+	}
+
+	class GithubRepositories extends Service {
+
+		/**
+		 * @constructor
+		 */
+		public function __construct( $config ){
+			$this->setURL( sprintf( 'http://github.com/api/v2/xml/repos/show/%s', $config['username'] ) );
+			$this->setURLTemplate( 'http://github.com/'.$config['username'].'/' );
+			$this->setItemTemplate('<li class="clearfix"><div class="metadata"><a href="{%url%}"><strong>{%name%}</strong></a></div><div class="infos">{%description%}</div></li>'."\n");
+			parent::__construct( $config );
+		}
+
+		/**
+		 * @return array
+		 */
+		public function populateItemTemplate( &$item ) {
+			return array(
+				'description' => $item->description,
+				'forks' => $item->forks,
+				'name' => $item->name,
+				'watchers' => $item->watchers,
+				'private' => $item->private,
+				'url' => $item->url,
+				'fork' => $item->fork,
+				'owner' => $item->owner,
+				'homepage' => $item->homepage,
+			);
+		}
+
+		/**
+		 * @return array
+		 */
+		public function getData() {
+			return parent::getData()->repository;
+		}
+
+		/**
+		 * Adds "github" to the box HTML class attribute
+		 * @return array
+		 */
+		public function populateBoxTemplate( $data ) {
+			return parent::populateBoxTemplate( $data ) + array( 'class' => $data['class'].' github');
 		}
 
 	}

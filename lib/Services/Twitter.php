d3209f2c30b2dec3a66fda0b61c43819c567e838
Rémi Prévost <remi@exomel.com>
2010-04-07 22:07:01 -0400

    Use search.twitter.com/search instead of twitter.com/search for hashtag links

--- lib/Services/Twitter.php
+++ lib/Services/Twitter.php
@@ -34,7 +34,7 @@
 		public function filterContent( $text ) {
 			$text = strip_tags( $text );
 			$text = preg_replace( '/(https?:\/\/[^\s\)]+)/', '<a href="\\1">\\1</a>', $text );
-			$text = preg_replace( '/\#([^\s\ \:\.\;\-\,\!\)\(\"]+)/', '<a href="http://twitter.com/search?q=%23\\1">#\\1</a>', $text );
+			$text = preg_replace( '/\#([^\s\ \:\.\;\-\,\!\)\(\"]+)/', '<a href="http://search.twitter.com/search?q=%23\\1">#\\1</a>', $text );
 			$text = preg_replace( '/\@([^\s\ \:\.\;\-\,\!\)\(\"]+)/', '@<a href="http://twitter.com/\\1">\\1</a>', $text );
 			$text = '<p>' . $text . '</p>';
 			return $text;

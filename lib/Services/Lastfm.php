8f7b3641f1f05e5d12e6500eb409a2a4c535ca31
Rémi Prévost <remi@exomel.com>
2010-05-26 19:05:12 -0400

    Update LastFM service version and available methods list

--- lib/Services/Lastfm.php
+++ lib/Services/Lastfm.php
@@ -4,9 +4,9 @@
 	/**
 	 * @classname LastFM
 	 * @description Fetch data from Last.fm
-	 * @version 1.1 (20090929)
+	 * @version 1.2 (20100526)
 	 * @author Rémi Prévost (exomel.com)
-	 * @methods LastFMWeeklyAlbums LastFMRecentTracks
+	 * @methods LastFMWeeklyAlbums LastFMRecentTracks LastFMTopAlbums
 	 */
 
 	class LastFM extends Service {

32c876926228b1ca7d0556785ab327919ba1cdb1
Rémi Prévost <remi@exomel.com>
2010-05-29 14:59:10 -0400

    Add YoutubeFavorites method (with YoutubeVideos)

--- lib/Services/Youtube.php
+++ lib/Services/Youtube.php
@@ -15,10 +15,8 @@
 		private $size;
 
 		public function __construct( $config ){
-			$config['url'] = sprintf( 'http://gdata.youtube.com/feeds/api/users/%s/uploads?v=2', $config['username'] );
-			$config['link'] = 'http://www.youtube.com/user/'.$config['username'].'/';
-			parent::__construct( $config );
 			$this->size = $config['size'];
+			parent::__construct( $config );
 			$this->setItemTemplate('<li class="clearfix"><a href="{{{link}}}"><img src="{{{image}}}" alt="{{{title}}}" /><strong>{{{title}}}</strong></a></li>'."\n");
 		}
 
@@ -27,18 +25,39 @@
 		 */
 		public function populateItemTemplate( &$item ) {
 			$media = $item->children('http://search.yahoo.com/mrss/');
-			$attrs = $media->group->thumbnail[0]->attributes();
-			$title = (string) $media->group->title;
-			$description = (string) $media->group->description;
-			$url_attrs = $media->group->player->attributes();
-			$url = $url_attrs['url'];
+
+			if ( $media->group->thumbnail ) {
+				$attrs = $media->group->thumbnail[0]->attributes();
+				$title = (string) $media->group->title;
+				$description = (string) $media->group->description;
+				$url_attrs = $media->group->player->attributes();
+				$url = $url_attrs['url'];
+			}
 
 			return parent::populateItemTemplate( $item ) + array(
-						'title' => $title,
-						'description' => $description,
-						'image' => $attrs['url'],
-						'size' => $item->size,
+				'image' => $attrs ? $attrs['url'] : '',
+				'size' => $item->size,
 			);
+		}
+
+	}
+
+	class YoutubeVideos extends Youtube {
+
+		public function __construct( $config ){
+			$config['url'] = sprintf( 'http://gdata.youtube.com/feeds/api/users/%s/uploads?v=2', $config['username'] );
+			$config['link'] = 'http://www.youtube.com/user/'.$config['username'].'/';
+			parent::__construct( $config );
+		}
+
+	}
+
+	class YoutubeFavorites extends Youtube {
+
+		public function __construct( $config ){
+			$config['url'] = sprintf( 'http://gdata.youtube.com/feeds/api/users/%s/favorites?v=2', $config['username'] );
+			$config['link'] = 'http://www.youtube.com/user/'.$config['username'].'#p/f';
+			parent::__construct( $config );
 		}
 
 	}
